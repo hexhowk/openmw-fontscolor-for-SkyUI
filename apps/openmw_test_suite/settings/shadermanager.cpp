73c1c3d904a74bb27bcfdc18c43d01d97864974b
cody glassman <glassmancody.info@gmail.com>
2022-04-15 17:10:04 -0700

    changelog and fix dummy tests for pipeline

--- apps/openmw_test_suite/settings/shadermanager.cpp
+++ apps/openmw_test_suite/settings/shadermanager.cpp
@@ -26,15 +26,4 @@ namespace
             f(path);
         }
     };
-
-    TEST_F(ShaderSettingsTest, load_empty_file)
-    {
-        const std::string content;
-
-        withSettingsFile(content, [this] (const auto& path) {
-            ShaderManager::get().load(path);
-
-            EXPECT_EQ(ShaderManager::get().getRoot(), YAML::Node());
-        });
-    }
 }
